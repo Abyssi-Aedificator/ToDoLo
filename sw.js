e1ee3a91825e4ec4c62c08772cd2a2708e7865a0
Abyssi-Aedificator <profilumpublicum@proton.me>
2026-06-21 19:23:06 -0400

    Version Alpha 0.1.0

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = 'todolo-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
